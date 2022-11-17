18daada03e7192bc0e05f945fcfa8f64c0307d39
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-11-17 20:06:29 +0100

    cleaning code

--- js/modules/clsData.js
+++ js/modules/clsData.js
@@ -18,18 +18,7 @@ class clsData {
       this.ACTUAL_LOCALSTORAGE.push(this.jsonValue);
     }
     console.log(this.ACTUAL_LOCALSTORAGE);
-    // this._updateKeys();
-    // this.ACTUAL_LOCALSTORAGE.reverse();
-    // console.log(this.ACTUAL_LOCALSTORAGE);
-    // console.log('obtainLocalData')
-    // console.log(this.i);
 }
-
-  updateKeys(){
-    // this.keys = Object.keys(localStorage);
-    // this.i = this.keys.length;
-  }
-
   /**
    * When all the data is loaded into our ACTUAL_LOCALSTORAGE, the next step is to mount all these elements into the last position taken.
    */
@@ -40,6 +29,7 @@ class clsData {
       this.newTaskToDo = new clsTask(element["text"], true, element["position"],this.i);
       this.newTaskToDo.mountDiv();
     });
+    this.ACTUAL_LOCALSTORAGE = [];
     // this._updateKeys();
     // console.log('mountTasks')
     // console.log(this.i);
@@ -49,6 +39,7 @@ class clsData {
  * Linked to the button showed on Screen, that task will generate all the elements (img and text) that will have the task.
  */
   newTask() {
+    this.i = new Date().getTime();
     var newInstance = new clsTask(undefined,undefined,undefined, this.i);
     newInstance.mountDiv();
     newInstance.linkDiv(newInstance.firstParent, newInstance.row);
@@ -62,38 +53,12 @@ class clsData {
    * When a task is made, we need to take it into our Local Storage Data. That's what this method does.
    */
   sendToLocalStorage() {
-
-    // console.log(this.ACTUAL_LOCALSTORAGE);
-
-    // // for(this.i, this.i<this.ACTUAL_LOCALSTORAGE.length;this.i++;){
-    // // };
-    // this.i = 0;
     this.ACTUAL_LOCALSTORAGE.forEach((element) => {
-      // console.log(element);
-      if(localStorage.getItem(this.i) == null){
-        localStorage.setItem(this.i, element);
-      }
-      // console.log(localStorage);
-      this.i++;
-    // });
-    // this._updateKeys();
-    // this.i = this.keys.length;
-    // console.log('sendToLocalStorage')
-    // console.log(this.i);
+      localStorage.setItem(this.i, element);
+     });
+    this.ACTUAL_LOCALSTORAGE = [];
+    console.log(this.ACTUAL_LOCALSTORAGE);
 
-        });
   }
-
-  removeElementfromArray(){
-    // this._updateKeys();
-    // this.i = this.keys.length;
-    // for (let i = 0; i < array.length; i++) {
-    //   this.
-      
-    // }
-    // console.log('removeElementfromArray')
-    // console.log(this.i);
-    // console.log(this.ACTUAL_LOCALSTORAGE);
-  };
 
 }
