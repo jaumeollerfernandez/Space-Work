6902aa8a816bea0971c30f8ce7ea27bf6a44ebd2
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-11-17 19:35:04 +0100

    refactoring clsData

--- js/modules/clsData.js
+++ js/modules/clsData.js
@@ -5,43 +5,44 @@ class clsData {
   constructor() {
     this.ACTUAL_LOCALSTORAGE = [];
     this.keys = Object.keys(localStorage);
-    this.i = 0;
+    this.i = new Date().getTime();
+    this.count = 0;
   }
   /**
    * When the page starts, it is required to obtain those LocalStorage Data in order to mount all the tasks from the last session. That will take all those elements, to put it into this.ACTUAL_LOCALSTORAGE
    */
   obtainLocalData() {
-    (this.keys = Object.keys(localStorage)), (this.i = this.keys.length);
-    while (this.i--) {
-      this.jsonValue = JSON.parse(localStorage.getItem(this.keys[this.i]));
+    (this.keys = Object.keys(localStorage)), (this.count = this.keys.length);
+    while (this.count--) {
+      this.jsonValue = JSON.parse(localStorage.getItem(this.keys[this.count]));
       this.ACTUAL_LOCALSTORAGE.push(this.jsonValue);
     }
-
-    this._updateKeys();
-    this.ACTUAL_LOCALSTORAGE.reverse();
     console.log(this.ACTUAL_LOCALSTORAGE);
-    console.log('obtainLocalData')
-    console.log(this.i);
+    // this._updateKeys();
+    // this.ACTUAL_LOCALSTORAGE.reverse();
+    // console.log(this.ACTUAL_LOCALSTORAGE);
+    // console.log('obtainLocalData')
+    // console.log(this.i);
 }
 
-  _updateKeys(){
-    this.keys = Object.keys(localStorage);
-    this.i = this.keys.length;
+  updateKeys(){
+    // this.keys = Object.keys(localStorage);
+    // this.i = this.keys.length;
   }
 
   /**
    * When all the data is loaded into our ACTUAL_LOCALSTORAGE, the next step is to mount all these elements into the last position taken.
    */
   mountTasks() {
-    this.i= 0;
+
+    // this.i= 0;
     this.ACTUAL_LOCALSTORAGE.forEach((element) => {
       this.newTaskToDo = new clsTask(element["text"], true, element["position"],this.i);
       this.newTaskToDo.mountDiv();
-      this.i++;
     });
-    this._updateKeys();
-    console.log('mountTasks')
-    console.log(this.i);
+    // this._updateKeys();
+    // console.log('mountTasks')
+    // console.log(this.i);
 }
 
 /**
@@ -53,7 +54,7 @@ class clsData {
     newInstance.linkDiv(newInstance.firstParent, newInstance.row);
     this.ACTUAL_LOCALSTORAGE.push(newInstance.jsonPlanet);
     this.sendToLocalStorage();
-    console.log('newTask')
+    console.log('newTask');
     console.log(this.i);
   }
 
@@ -61,13 +62,12 @@ class clsData {
    * When a task is made, we need to take it into our Local Storage Data. That's what this method does.
    */
   sendToLocalStorage() {
-    console.log(this.ACTUAL_LOCALSTORAGE);
 
-    // for(this.i, this.i<this.ACTUAL_LOCALSTORAGE.length;this.i++;){
+    // console.log(this.ACTUAL_LOCALSTORAGE);
 
-
-    // };
-    this.i = 0;
+    // // for(this.i, this.i<this.ACTUAL_LOCALSTORAGE.length;this.i++;){
+    // // };
+    // this.i = 0;
     this.ACTUAL_LOCALSTORAGE.forEach((element) => {
       // console.log(element);
       if(localStorage.getItem(this.i) == null){
@@ -75,23 +75,24 @@ class clsData {
       }
       // console.log(localStorage);
       this.i++;
-    });
-    this._updateKeys();
-    this.i = this.keys.length;
-    console.log('sendToLocalStorage')
-    console.log(this.i);
+    // });
+    // this._updateKeys();
+    // this.i = this.keys.length;
+    // console.log('sendToLocalStorage')
+    // console.log(this.i);
 
+        });
   }
 
-  removeElementfromArray(pID){
-    this._updateKeys();
-    this.i = this.keys.length;
+  removeElementfromArray(){
+    // this._updateKeys();
+    // this.i = this.keys.length;
     // for (let i = 0; i < array.length; i++) {
     //   this.
       
     // }
-    console.log('removeElementfromArray')
-    console.log(this.i);
+    // console.log('removeElementfromArray')
+    // console.log(this.i);
     // console.log(this.ACTUAL_LOCALSTORAGE);
   };
 
